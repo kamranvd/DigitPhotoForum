19ce8b4e3f86ae2abc59dbacbcd5835fc6def9af
Kamran <kamran@lasysadmin.com>
2025-06-09 15:50:43 -0700

    created the app file

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { connectDB } = require('./config/db'); // Import connectDB function
+const path = require('path');
+
+// Load environment variables
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Body parser for JSON requests
+app.use(cors()); // Enable CORS for all routes
+
+// Test route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+
+
+// Define the port to listen on
+const PORT = process.env.PORT || 5000;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
